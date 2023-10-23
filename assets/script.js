8eaee43b6457d4d44dadb8f23810930ff9f07e4b
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-10-23 17:48:53 -0400

    added for applyiong past present and future classes for the time blocks and added and event listener for the save button

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -19,14 +13,54 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+ 
 });
 
+//applying past, present and future class to the time blocks. 
+var currentHour = dayjs().hour();
+
+$(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id").replace("hour-", ""));
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+    } else {
+        $(this).addClass("future");
+    }
+});
+
+
+
+// event listener for the save button on right side of page
+  $(".saveBtn").on("click", function() {
+    var userInput = $(this).siblings(".description").val();
+    var timeBlockId = $(this).parent().attr("id");
+    
+    localStorage.setItem(timeBlockId, userInput);
+  });
+
+
+function loadSavedData() {
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    var savedData = localStorage.getItem(timeBlockId);
+    if (savedData) {
+      $(this).find(".description").val(savedData);
+    }
+  });
+  console.log()
+}
+
+// calling function to load saved data 
+loadSavedData();
+
+ // code to display the current date in the header of the page.
 function updateDate() {
   var dateDisplay = document.getElementById("currentDay");
   var currentDate = new Date();
   var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   var formattedDate = currentDate.toLocaleDateString(undefined, options);
-  dateDisplay.textContent = formattedDate;
+  dateDisplay.textContent =  formattedDate;
 }
 updateDate();
