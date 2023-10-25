f67103889921f16547a3d5519067c90aac8e7d65
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-10-24 23:56:43 -0400

    deleted commented out top sections

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-
-
- 
-});
-
 var currentHour = dayjs().hour();
 
 // looping through blocks to see if they match up to current hour 
